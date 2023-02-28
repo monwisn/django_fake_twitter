0712466b11d20eab8520b630cdf4319023e91140
Monika Wiśniewska <wisniewska.monika00@gmail.com>
2023-02-28 23:55:14 +0100

    update profile picture

--- main/views.py
+++ main/views.py
@@ -327,7 +327,7 @@ def ai_chat(request):
         return redirect('main:home')
 
 
-openai.api_key = 'YOUR_OPENAI_API_KEY'
+openai.api_key = 'YOUR_API_KEY'
 
 
 # Generating response from OpenAI Library
